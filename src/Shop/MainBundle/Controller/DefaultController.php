0429e6b3a797e9a05d93e9e1280a7e9840821aaf
zgm92 <zgoda.marcin@gmail.com>
2015-02-15 22:21:37 +0100

    Modified HomePageView

--- src/Shop/MainBundle/Controller/DefaultController.php
+++ src/Shop/MainBundle/Controller/DefaultController.php
@@ -9,7 +9,24 @@ class DefaultController extends Controller
     public function indexAction()
     {
 //		$this->get('session')->set('my_name', $name);
+
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository("ShopMainBundle:Movie");
+
+        $collectionAllMovies = $repository->findAll();
+        $collectionMostPopularMovies = $repository->findAll();
+        $collectionMostReviewMovies = $repository->findAll();
+
+        return $this->render(
+            'ShopMainBundle:Default:index.html.twig',
+            array(
+                'allMovies' => $collectionAllMovies,
+                'mostPopularMovies' => $collectionMostPopularMovies,
+                'mostReviewMovies' => $collectionMostReviewMovies,
+            )
+        );
 		
-        return $this->render('ShopMainBundle:Default:index.html.twig');
+//        return $this->render('ShopMainBundle:Default:index.html.twig');
     }
 }
