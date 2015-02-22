7447fb3dfc058a806a32a6b082217e394d0f323d
Damian Wtorek <damian.wtr@gmail.com>
2015-02-22 23:26:05 +0100

    c

--- src/Shop/MainBundle/Controller/UserController.php
+++ src/Shop/MainBundle/Controller/UserController.php
@@ -1,32 +1,17 @@
 <?php
-
 namespace Shop\MainBundle\Controller;
-
 use Doctrine\Common\Collections\ArrayCollection;
 use Shop\MainBundle\Entity\Cart;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-/**
- * Class UserController
- * @package Shop\MainBundle\Controller
- */
 class UserController extends Controller
 {
-
-    /**
-     * @param $userId
-     * @return int
-     */
     public function getCartCount($userId)
     {
         $em = $this->getDoctrine()->getManager();
         $repository = $em->getRepository("ShopMainBundle:Cart");
         $tmpCart = $repository-> findBy(array('userId' => $userId ));
-
         $repo= $em->getRepository("ShopMainBundle:Movie");
-
         $cartCount = 0;
-
         foreach( $tmpCart as &$item)
         {
             $movies = $repo-> findBy(array('id' => $item->getMovies()));
@@ -37,28 +22,18 @@ class UserController extends Controller
         }
         return $cartCount;
     }
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
     public function indexAction()
     {
         $tmp = $this->getUser();
         $userId = $tmp->getId();
-
         $em = $this->getDoctrine()->getManager();
         $repository = $em->getRepository("ShopMainBundle:Order");
         $collectionAllOrders = $repository-> findBy(array('userId' => $userId ));
-
         return $this->render(
             'ShopMainBundle:User:index.html.twig',
             array(
-               'orders' => $collectionAllOrders,
+                'orders' => $collectionAllOrders,
             )
         );
     }
-
-
-
-
 }
